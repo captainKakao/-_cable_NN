7a6d0fab16d992b09edfcc826b74158f905bfd44
Petr Zhdanov <peter.zhdanov.n@gmail.com>
2024-03-03 22:48:29 +0300

    new version

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import keras
 from keras.layers import Dense, Input
 from keras import Model
+import tensorflow as tf
 from numpy import genfromtxt
 import os
 
-file_src = "C:/Users/Professional/Documents/pycharm/Static_cable_data_creator/files/"
+# file_src = "C:/Users/Professional/Documents/pycharm/Static_cable_data_creator/files/"
+file_src = "F:/files_NN_new/"
 old = 0
 d = 1000
 k = 0
@@ -19,8 +21,10 @@ for x in os.listdir(file_src):
         old = int(num)
 
 loaded_arr = []
-in_list = [[0 for i in range(11)] for j in range(old+1)]
-out_list = [[0 for i in range(33)] for j in range(old+1)]
+in_list1 = [[0 for i in range(10)] for j in range(old+1)]
+out_list1 = [[0 for i in range(33)] for j in range(old+1)]
+in_list = np.zeros((old+1, 10))
+out_list= np.zeros((old+1, 33))
 
 
 for x in os.listdir(file_src):
@@ -41,9 +45,11 @@ for x in os.listdir(file_src):
 
 
 model = keras .Sequential()
-model.add(Dense(units=11, input_shape=(11,), activation='tanh'))
-model.add(Dense(units=8, activation='tanh'))
-model.add(Dense(units=8, activation='tanh'))
+model.add(Dense(units=10, input_shape=(10,), activation='tanh'))
+model.add(Dense(units=32, activation='tanh'))
+model.add(Dense(units=32, activation='tanh'))
+# model.add(Dense(units=32, activation='tanh'))
+# model.add(Dense(units=32, activation='tanh'))
 model.add(Dense(units=33, activation='tanh'))
 print(model.summary())
 model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.001))
@@ -51,8 +57,11 @@ model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.001))
 
 
 print(1)
-history = model.fit(in_list, out_list, epochs=150, verbose=True)
+history = model.fit(in_list, out_list, epochs=100, batch_size=16, validation_split=0.2, verbose=True)
 print(2)
+
+# model.evaluate()
+
 plt.plot(history.history['loss'])
 plt.grid(True)
 plt.show()
